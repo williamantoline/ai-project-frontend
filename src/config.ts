9349def626fd6e0fa1b9ac857cc079055782cc5b
William Antoline <65105895+williamantoline@users.noreply.github.com>
2023-05-25 12:53:29 +0700

    change api endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const endpoint = 'http://127.0.0.1:3002';
+export const endpoint = 'https://ai-project-backend-staging.up.railway.app';
 export const enckey = "jkmkFUNMJPrF4RP7kwzu3q5fiDN3uB7ScjF858Jd4OGv0CbUuAN";
